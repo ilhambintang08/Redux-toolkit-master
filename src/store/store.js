9bf146e45cfc1d872f65d2607245d3f9d10c2044
ilhambintang08 <108867460+ilhambintang08@users.noreply.github.com>
2022-07-07 16:37:19 +0700

    Add files via upload

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './counter'
+import burgerSlice from './burger'
+import productSlice from './product'
+
+const store = configureStore({
+
+    reducer: {
+        counter: counterSlice.reducer,
+        burger: burgerSlice.reducer,
+        product: productSlice.reducer
+    }  
+})
+
+export default store
